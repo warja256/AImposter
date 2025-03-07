e5720e8de072a8ad3423ee598955a4aabc513a68
Maria <mariatimina2005@gmail.com>
2025-03-08 01:15:20 +0300

    Добавлена конфигурация swagger

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,30 @@ app.use(express.json());
 
 
 
+const swaggerOptions = {
+    //параметры openapi 3.0
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'AImposter Game API',
+        version: '1.0.0',
+        description: 'API documentation for the AImposter Game',
+      },
+      //сервер разработки
+      servers: [
+        {
+          url: 'http://localhost:3000',
+          description: 'Development server',
+        },
+      ],
+    },
+    apis: ['./server.js'],
+  };
+  //генерируется документация на основе аннотаций в коде
+  const swaggerDocs = swaggerJsdoc(swaggerOptions);
+  //чтобы показывать сгенерированную документацию
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 
 
 const PORT = 3000;
