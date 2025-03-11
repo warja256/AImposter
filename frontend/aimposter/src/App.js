15c10de4b242ccc284b00d583e168b04cf654f1e
Alx1307 <ekhmara75@yandex.ru>
2025-03-12 01:35:07 +0300

    добавлен экран выбора ответа от ии

--- frontend/aimposter/src/App.js
+++ frontend/aimposter/src/App.js
@@ -4,11 +4,12 @@ import HumanRoleScreen from './components/roles/HumanRoleScreen';
 import MafiaScreen from './components/roles/MafiaScreen';
 import WelcomeScreen from './components/welcome/WelcomeScreen.js';
 import AiChatScreen from './components/ai_chat/AiChat.js';
+import AiResponses from './components/ai_chat/AiResponses.js';
 
 function App() {
   return (
     <div>
-      <AiChatScreen/> 
+      <WelcomeScreen/> 
     </div>
   );
 }
