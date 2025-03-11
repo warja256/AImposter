d676ae511dc375f8cd8839f933a96ef2f26bfabd
Alx1307 <ekhmara75@yandex.ru>
2025-03-11 23:29:06 +0300

    добавлен экран запроса для мафии

---
+++ frontend/aimposter/src/components/ai_chat/AiChat.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import './AiChat.css';
+import '../header.css';
+import logo from '../../assets/images/logo.png';
+import info from '../../assets/images/info.png';
+
+const AiChatScreen = () => {
+  const [countdown, setCountdown] = useState(15); // Таймер
+  const [roomCode, setRoomCode] = useState('9090');
+
+  const formatTime = (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes.toString().padStart(2, '0')}:${remainingSeconds.toString().padStart(2, '0')}`;
+  };
+
+  useEffect(() => {
+    let interval = null;
+
+    if (countdown !== 0) {
+      interval = setInterval(() => {
+        setCountdown(countdown => countdown - 1);
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [countdown]);
+
+  return (
+    <div className="ai-chat-screen">
+      
+      <div className="logo-container">
+        <img src={logo} alt="AImposter Logo" />
+        <span className="header-title">AImposter</span>
+      </div>
+
+      <div className="chat-box">
+        <div className="panel">
+          <img src={info} alt="Info" className="info" />
+          <div className="timer">{formatTime(countdown)}</div>
+          <div className="code-panel">
+            <p className="code-label">CODE:</p>
+            <p className="code-value">{roomCode}</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default AiChatScreen;
