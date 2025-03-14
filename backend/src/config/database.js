c1625a4164203835a692bd5e90fbe379644d9597
Maria <mariatimina2005@gmail.com>
2025-03-14 17:06:41 +0300

    Добавление создание игрока при создании комнаты (не работает)

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize('AImposter', 'postgres', 'Baran9940', {
-// const sequelize = new Sequelize('AImposter', 'postgres', 'fde2a1c7', {
+//const sequelize = new Sequelize('AImposter', 'postgres', 'Baran9940', {
+const sequelize = new Sequelize('AImposter', 'postgres', 'fde2a1c7', {
     host: 'localhost',
     port: 5432,
     dialect: 'postgres',
