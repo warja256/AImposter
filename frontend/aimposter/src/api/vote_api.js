5f5fa41347aed66afb38f763a1ec572ccbb5fb21
Varya Kush <barbarakushaeva@gmail.com>
2025-04-04 18:30:23 +0300

    написана функция playerSetVote

--- frontend/aimposter/src/api/vote_api.js
+++ frontend/aimposter/src/api/vote_api.js
@@ -1,1 +1,16 @@
+import axios from "axios";
+
 const API_URL = 'http://localhost:8080/api';
+export const playerSetVote = async (voterId, targetId, roomCode) => {
+  try {
+    const response = await axios.post(`${API_URL}/vote/${roomCode}`, {
+      voterId,
+      targetId
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Ошибка при отправке голоса:", error.response?.data || error.message);
+    throw error;
+  }
+};
+
