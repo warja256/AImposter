58a6eee43b38dbc58f76e6e47752e59959d1489d
golikova_elizaveta <golikovaliza385@gmail.com>
2025-04-04 19:06:45 +0300

    ф-я голосования мафии

--- frontend/aimposter/src/api/vote_api.js
+++ frontend/aimposter/src/api/vote_api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const API_URL = 'http://localhost:8080/api';
+
 export const playerSetVote = async (voterId, targetId, roomCode) => {
   try {
     const response = await axios.post(`${API_URL}/vote/${roomCode}`, {
@@ -14,3 +15,18 @@ export const playerSetVote = async (voterId, targetId, roomCode) => {
   }
 };
 
+export const mafiaSetVote = async (voterId, targetId, roomCode) => {
+  try {
+    const response = await axios.post(`${API_URL}/vote/mafia/${roomCode}`, {
+      voterId,
+      targetId
+    });
+    return {
+      status: response.status,
+      data: response.data,
+    };
+  } catch (error) {
+    console.error("Ошибка при отправке голоса:", error.response?.data || error.message);
+    throw error;
+  }
+};
