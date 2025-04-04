d47345ea82a081d58a66f3a11a2489cf25e94ac7
Varya Kush <barbarakushaeva@gmail.com>
2025-04-04 18:37:13 +0300

    добавлены удалённые Катины файлы

---
+++ frontend/aimposter/src/api/role_api.js
@@ -0,0 +1,18 @@
+const API_URL = 'http://localhost:8080/api';
+
+export const getMafiaId = async () => {
+    try {
+        const response = await fetch(`${API_URL}/rooms/chooseMafia/${roomCode}`);
+
+        if (!response.ok) {
+            throw new Error(`Ошибка! Статус: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data;
+    }
+    catch (error) {
+        console.error("Ошибка получения id мафии:", error);
+        throw error;
+    }
+};
